af4d6e8be60709f9cedbf089786a728c40079684
RenanKanu <renankanu@gmail.com>
2020-06-08 22:06:14 -0300

    call TelaInicial

--- app/src/main/java/com/example/digitalmarketlist/LoginActivity.java
+++ app/src/main/java/com/example/digitalmarketlist/LoginActivity.java
@@ -1,14 +1,31 @@
 package com.example.digitalmarketlist;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class LoginActivity extends AppCompatActivity {
+    private Button botaoConfirmar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        botaoConfirmar = findViewById(R.id.btt_confirmar);
+        botaoConfirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chamarTelaInicial();
+            }
+        });
+    }
+
+    public void chamarTelaInicial() {
+        Intent intent = new Intent(this, TelaInicialActivity.class);
+        startActivity(intent);
     }
 }
